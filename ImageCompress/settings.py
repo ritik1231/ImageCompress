eabab2ef59cf55f4b6400c769eaa9bd4fc18a26e
Ritik Bansal <56888511+ritik1231@users.noreply.github.com>
2024-06-28 20:59:10 +0530

    Update settings.py

--- ImageCompress/settings.py
+++ ImageCompress/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ygw*-6il#u%lh92xp-#4(4#-vm87-0av-+q13vn=_e#=n#s#+o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
